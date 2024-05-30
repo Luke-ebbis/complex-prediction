88d7926155052d3eb040f3d9e95caf94af82a855
Sibbe Bakker <sibbe.l.bakker@gmail.com>
2024-05-30 09:45:37 +0200

    make sure logs go to the right place

--- workflow/scripts/jobscript.sh
+++ workflow/scripts/jobscript.sh
@@ -5,14 +5,13 @@
 # Initial working directory:
 #SBATCH -D ./
 # Job name
-#SBATCH -J test_gpu_combfold
+#SBATCH -J CombfoldWorkflow
 #
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:a100:1
 #SBATCH --cpus-per-task=18
-#SBATCH --mem=12800
+#SBATCH --mem=32000
 #SBATCH --time=24:00:00  # Specify the runtime (24 hours in this case)
 #
 mkdir logs/slurm -p
-
 srun workflow/scripts/slurmer.sh
