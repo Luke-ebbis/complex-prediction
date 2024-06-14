52b9198d296f573ad7c36e40b53c3a3dcd8e191b
Sibbe Bakker <sibbe.l.bakker@gmail.com>
2024-06-14 15:44:25 +0200

    Add settings to use the gpu

--- workflow/scripts/dependencies/setup-cuda.sh
+++ workflow/scripts/dependencies/setup-cuda.sh
@@ -1,4 +1,8 @@
 echo 'cuda setup'
 pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 python -c "import jax; print(f'Jax backend: {jax.default_backend()}')"
+export CUDA_HOME=/usr/local/cuda-11.2
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export PATH=$CUDA_HOME/bin:$PATH
+
 exit
